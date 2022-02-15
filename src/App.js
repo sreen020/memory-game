6ee6e29c05cc2d022bf6db6a7ca90e8fe2e5135c
SR <sjoerd@MacBook-Pro-van-SR.local>
2022-02-15 23:33:27 +0100

    setup project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory game</h1>
+      <button>New game</button>
+    </div>
+  );
+}
+
+export default App;
